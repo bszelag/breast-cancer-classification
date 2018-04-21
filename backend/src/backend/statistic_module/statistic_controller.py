f04d097842e74b4b31731e4978a8c62a0ea46613
MGniew <m.f.gniewkowski@gmail.com>
2018-04-21 23:47:41 +0200

    basic method defined

--- backend/src/backend/statistic_module/statistic_controller.py
+++ backend/src/backend/statistic_module/statistic_controller.py
@@ -1,8 +1,19 @@
 from src.backend import app, db
 from flask import Blueprint, request, abort, jsonify
 
-statistics = Blueprint("statistic", __name__,  url_prefix='/stats)
+statistics = Blueprint("statistic", __name__, url_prefix='/stats')
 
-@statistics.route("<algorithm_name>/history", method=["GET"])
+
+@statistics.route("/<algorithm_name>/history", method=["GET"])
 def get_history(algorithm_name):
+    return "TODO"
+
+
+@statistics.route("/<algorithm_name>/total_accuracy", method=["GET"])
+def get_total_accuracy(algorithm_name):
+    return "TODO"
+
+
+@statistics.route("/<algorithm_name>/total_accuracy/reset", method=["GET"])
+def get_total_accuracy(algorithm_name):
     return "TODO"
