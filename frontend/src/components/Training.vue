73e5f6b4ecb0eb4c9e72c16865667093508fe314
bszelag <b.z.szelag@gmail.com>
2018-05-01 16:47:01 +0200

    save

--- frontend/src/components/Training.vue
+++ frontend/src/components/Training.vue
@@ -7,6 +7,15 @@
             <b-form-radio-group required id="radios" v-model="form.classification" :options="classificationOptions">
             </b-form-radio-group>
           </b-form-group>
+          <b-form-group label="Options" label-for="args.options" description="Additional options">
+            <div v-if="form.classification === 'bayes'">
+              <b-form-radio-group id="bayesOptions" v-model="form.args.options['dist']" :options="distOptions">
+              </b-form-radio-group>
+              {{ form.args.options ? form.args.options : 'nuuuu' }}
+            </div>
+            <div v-if="form.classification === 'svm'">svm opt</div>
+            <div v-if="form.classification === 'tree'">tree opt</div>
+          </b-form-group>
           <b-form-group label="Training data" label-for="file" description="Select training data">
             <b-form-file ref="fileinput" required v-model="form.file" :state="Boolean(form.file)" placeholder="Choose a file..."></b-form-file>
             <div class="mt-3">Selected file: {{form.file && form.file.name}}</div>
@@ -26,12 +35,50 @@ export default {
     return {
       form: {
         classification: '',
+        args: {
+          options: {}
+        },
         file: null
       },
       classificationOptions: [
         {text: 'Naive Bayes', value: 'bayes'},
         {text: 'Decision Tree', value: 'tree'},
         {text: 'SVM', value: 'svm'}
+      ],
+      argsOptions: {
+        'bayes': {'dist': null},
+        'svm': {'kernel': null, 'C': null, 'gamma': null},
+        'tree': {'criterion': null, 'min_samples_split': null, 'splitter': null}
+      },
+      distOptions: [
+        {text: 'Gaussian Naive Bayes', value: 'GaussianNB'},
+        {text: 'Multinominal Naive Bayes', value: 'MultinominalNB'}
+      ],
+      kernelOptions: [
+        {text: 'Linear', value: 'linear'},
+        {text: 'Poly', value: 'poly'},
+        {text: 'RBF', value: 'rbf'},
+        {text: 'Signoid', value: 'signoid'},
+        {text: 'Precomputed', value: 'precomputed'}
+      ],
+      cOptions: [
+        {text: '10', value: 10},
+        {text: '100', value: 100},
+        {text: '1000', value: 1000}
+      ],
+      gammaOptions: [
+        {text: '0.1', value: 0.1},
+        {text: '0.01', value: 0.01},
+        {text: '0.001', value: 0.001},
+        {text: '0.0001', value: 0.0001}
+      ],
+      criterionOptions: [
+        {text: 'Gini', value: 'gini'},
+        {text: 'Entropy', value: 'entropy'}
+      ],
+      splitterOptions: [
+        {text: 'Best', value: 'best'},
+        {text: 'Random', value: 'random'}
       ]
     }
   },
