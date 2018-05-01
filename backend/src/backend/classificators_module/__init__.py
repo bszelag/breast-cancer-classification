aefedb3fb927cbc2a5f9010bab6e9cbd27c084f7
MGniew <m.f.gniewkowski@gmail.com>
2018-05-01 20:27:02 +0200

    bp

--- backend/src/backend/classificators_module/__init__.py
+++ backend/src/backend/classificators_module/__init__.py
@@ -6,5 +6,5 @@ for k in algorithms:
     record = db.models.find_one({"_id": k})
     if record is not None:
         algorithms[k].model = pickle.loads(record["pickle"])
-        algorithms[k].mask_ = record["mask"]
-
+        if "mask" in record:
+            algorithms[k].mask_ = record["mask"]
