bb6a76cd0fd982554f54359c37e581abe30665c3
MGniew <m.f.gniewkowski@gmail.com>
2018-05-15 21:44:00 +0200

    config.py

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 DEBUG = True
 SESSION_COOKIE_HTTPONLY = False
-DB_URL = "mgniewkowski.nazwa.pl:27017"
+DB_URL = "localhost:27017"
 DB_NAME = "breast_cancer_db"
 DB_USER = "flask_app"
-DB_PASS = "5QNgBXuWeqcCaFsf"
+DB_PASS = "secretpass"
